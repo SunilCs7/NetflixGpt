a09b7a8c744d73ba60c68403913119f0df923dbd
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-03-09 20:26:24 +0530

    Routing setup and work done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-red-800">
-      NetflixGpt, get started
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
