14c19ccf4d0f79ccdf79d2371212e474369c4373
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-03-09 20:58:54 +0530

    Build header

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        {" "}
+        <img
+          src="https://thefatork.store/cdn/shop/articles/netflix.jpg?v=1669877376"
+          alt="background img"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
