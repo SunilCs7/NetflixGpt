4b1b94a67aa378c559fa830a090e30f0ed9dd2af
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-03-15 00:01:39 +0530

    Setup redux tool kiit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import Body from "./components/Body";
+import appStore from "./utils/appStore";
 
 function App() {
-  return <Body />;
+  return (
+    <Provider store={appStore}>
+      <Body />
+    </Provider>
+  );
 }
 
 export default App;
